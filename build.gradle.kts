711f85a899be13b08320b19ffa762e826ce09838
Soapy7261 <81933267+Soapy7261@users.noreply.github.com>
2026-01-10 20:09:13 -0500

    add graal to build.gradle.kts
    Sadly I can't get GraalVM 21 on NixOS, so it's time to rely on github actions I guess.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,20 @@
 // adding this helps intellij with resolving repositories to be able to download library docs and sources
 plugins {
     bluemap.base
+    id("org.graalvm.buildtools.native") version "0.11.1"
+}
+
+graalvmNative {
+    toolchainDetection.set(true)
+}
+
+graalvmNative {
+    binaries {
+        named("main") {
+            javaLauncher.set(javaToolchains.launcherFor {
+                languageVersion.set(JavaLanguageVersion.of(21))
+                vendor.set(JvmVendorSpec.matching("Oracle Corporation"))
+            })
+        }
+    }
 }
